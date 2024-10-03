d176d6bbd973c73adcdb54f55804e4fedfe34fa2
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-10-03 08:10:43 +0700

    Memodifikasi program untuk tahun 2000 ( menambahkan if)

--- Pemilihan2Percobaan1_19.java
+++ Pemilihan2Percobaan1_19.java
@@ -7,7 +7,10 @@ public class Pemilihan2Percobaan1_19{
     tahun = sc19.nextInt();
     if ((tahun % 4) == 0){
         if((tahun % 100) !=0){
-            System.out.println("Tahun Kabisat");
+           if((tahun % 400) ==0){
+           } else {
+            System.out.println(" Tahun kabisat ");
+           }
         } else{
         System.out.println("bukan tahun kabisat");
     }
